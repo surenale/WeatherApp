31349f96b7440086b2c75108a521a294a3520a4a
surenGit <surenale69@gmail.com>
2022-06-13 23:12:58 +0545

    front-end and routing of weather interface

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+// public static path
+const static_path = path.join(__dirname,"../public");
+app.use(express.static(static_path));
+
+
+// routing
+app.get("/",(req,res) => {
+    res.send("welcome to suren ale channel");
+})
+
+app.get("/about",(req,res) => {
+    res.send("welcome to about page");
+})
+
+app.get("/weather",(req,res) => {
+    res.send("welcome to weather forcast page");
+})
+
+app.get("*",(req,res) => {
+    res.send("404 error page oops");
+})
+
+app.listen(port, function(){ 
+    console.log(`listening to port at ${port}`);
+})
