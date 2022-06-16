9c82474fa15a07cbf2233eff6adf17ff8eb3faef
surenGit <surenale69@gmail.com>
2022-06-16 11:02:59 +0545

    getting temperature data from API and displaying the temerature mood using awesome font

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,6 @@ const city_name = document.getElementById('city_name');
 const temp = document.getElementById('temp');
 const temp_status = document.getElementById('temp_status');
 
-
 const getInfo = async (event) => {
     event.preventDefault();
 
@@ -20,9 +19,24 @@ const getInfo = async (event) => {
             const data = await response.json();
             const arrData = [data];
 
-            // Wcity_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
-            // temp.innerText = arrData[0].main.temp;
-            // temp_status.innerText = arrData[0].eather[0].main;
+            city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
+            temp.innerText = Math.round(arrData[0].main.temp - 273.15);
+            const tempMood = arrData[0].weather[0].main;
+            
+            //condition to check the sunny or cloudy
+            if (tempMood === "Clear") {
+                temp_status.innerHTML =
+                    "<i class = 'fas fa-sun' style = 'color:#eccc68;'></i>";
+            } else if (tempMood === "Clouds") {
+                temp_status.innerHTML =
+                    "<i class = 'fas fa-cloud' style = 'color:#f1f2f6;'></i>";
+            } else if (tempMood === "Rain") {
+                temp_status.innerHTML =
+                    "<i class = 'fas fa-cloud-rain' style = 'color:#a4b0be;'></i>";
+            } else {
+                temp_status.innerHTML =
+                    "<i class = 'fas fa-sun' style = 'color:#eccc68;'></i>";
+            }
 
         } catch {
             city_name.innerText = `Plz enter the city name properly`;
