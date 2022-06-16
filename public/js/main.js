8430eebd2a9d6d3fcecc1f767842f112f0b81869
surenGit <surenale69@gmail.com>
2022-06-16 12:51:50 +0545

    getting current date

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
 const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
 const city_name = document.getElementById('city_name');
-const temp = document.getElementById('temp');
+const temp_real_val = document.getElementById( 'temp_real_val');
 const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
 
 const getInfo = async (event) => {
     event.preventDefault();
@@ -10,6 +11,7 @@ const getInfo = async (event) => {
     let cityVal = cityName.value;
     if (cityVal === '') {
         city_name.innerText = `Plz write the city name before search`;
+        datahide.classList.add('data_hide');
     }
 
     else {
@@ -20,9 +22,9 @@ const getInfo = async (event) => {
             const arrData = [data];
 
             city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
-            temp.innerText = Math.round(arrData[0].main.temp - 273.15);
+            temp_real_val.innerText = Math.round(arrData[0].main.temp - 273.15);
             const tempMood = arrData[0].weather[0].main;
-            
+
             //condition to check the sunny or cloudy
             if (tempMood === "Clear") {
                 temp_status.innerHTML =
@@ -37,9 +39,11 @@ const getInfo = async (event) => {
                 temp_status.innerHTML =
                     "<i class = 'fas fa-sun' style = 'color:#eccc68;'></i>";
             }
+            datahide.classList.remove('data_hide');
 
         } catch {
             city_name.innerText = `Plz enter the city name properly`;
+            datahide.classList.add('data_hide');
         }
 
     }
